9989e701ca256d4672d6aae214fed86a81dfbe27
Daniel Ramirez <danielfr1987@gmail.com>
2023-06-01 22:20:03 -0600

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/daniel-ramirez/home.git";
+const repoURL = "git@github.com:daniel-ramirez/home.git";
 
 ghpages.publish(
   pathname,
